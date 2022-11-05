bb47c16603e324959230f9e424aa8c3d03676106
AslanGoldenhour <aslan@tradery.io>
2022-11-05 05:11:44 -0700

    Added dune util for querying nft collection sales history

--- apps/price-feeder-api/src/main.ts
+++ apps/price-feeder-api/src/main.ts
@@ -1,5 +1,6 @@
 import { MongoClient } from 'mongodb';
 import * as express from 'express';
+import * as dune from './utils/dune';
 
 const STAT_TYPES = ['TAMI', 'MARKET_CAP'] as const;
 
@@ -30,7 +31,7 @@ app.get('/api/collections', async (req, res) => {
 // Get stat for collection
 //========================
 app.get('/api/stats', async (req, res) => {
-  const { chainId, address, stat } = req.query;
+  const { chainId = '1', address, stat } = req.query;
 
   if (
     typeof chainId !== 'string' ||
@@ -75,7 +76,7 @@ app.get('/api/feeds', async (req, res) => {
 // Create new stats feed
 //================
 app.post('/api/stats', async (req, res) => {
-  const { chainId, address, stat } = req.query;
+  const { chainId = '1', address, stat } = req.query;
 
   if (
     typeof chainId !== 'string' ||
@@ -99,9 +100,32 @@ app.post('/api/stats', async (req, res) => {
   }
 
   console.log({ chainId, address, statType });
+  
+  // Check to see if we have this collection saved in the db
+  const db = await feeder();
+  const query = { 
+    chainId: chainId,
+    address: address
+  };
+  const collection = await db.collection("NFTCollections").find(query).toArray();
+
+  if (collection.length === 0) {
+    // no collection found... let's create one
+    const insertCollection = await db.collection("NFTCollections").insertOne(query);
+    
+    // Get sales hisory and create one record per transaction
+    const salesHistory = await dune.getNFTCollectionSales(address, 10);
+    
+  } else {
+    // found the collection... let's just save the latest transactions
+    console.log('collection exists');
+
+    // First get the number of transactions already saved and use that for the offset
+  }
+  console.log(collection);
 
   res.send({
-    status: 123,
+    response: collection,
   });
 });
 
