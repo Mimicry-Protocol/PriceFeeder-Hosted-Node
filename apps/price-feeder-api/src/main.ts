22e686f6862a470f40a70a88ea6a068776e0af9a
AslanGoldenhour <aslan@tradery.io>
2022-11-06 01:35:19 -0700

    /api/stats POST working as designed

--- apps/price-feeder-api/src/main.ts
+++ apps/price-feeder-api/src/main.ts
@@ -1,6 +1,7 @@
-import { MongoClient } from 'mongodb';
+import { Collection, MongoClient, ObjectId } from 'mongodb';
 import * as express from 'express';
 import * as dune from './utils/dune';
+import * as reservoir from './utils/reservoir';
 
 const STAT_TYPES = ['TAMI', 'MARKET_CAP'] as const;
 
@@ -38,9 +39,7 @@ app.get('/api/stats', async (req, res) => {
     typeof address !== 'string' ||
     typeof stat !== 'string'
   ) {
-    res.status(400).send({
-      error: 'Invalid query parameters',
-    });
+    handleError(res, 400, 'Invalid query parameters');
     return;
   }
 
@@ -48,9 +47,7 @@ app.get('/api/stats', async (req, res) => {
   const statType = STAT_TYPES[statNumber];
 
   if (!statType) {
-    res.status(400).send({
-      error: 'Invalid stat type',
-    });
+    handleError(res, 400, 'Invalid stat type');
     return;
   }
 
@@ -76,65 +73,122 @@ app.get('/api/feeds', async (req, res) => {
 // Create new stats feed
 //================
 app.post('/api/stats', async (req, res) => {
-  const { chainId = '1', address, stat } = req.query;
+  try {
+    const { chainId = '1', address, stat } = req.query;
 
-  if (
-    typeof chainId !== 'string' ||
-    typeof address !== 'string' ||
-    typeof stat !== 'string'
-  ) {
-    res.status(400).send({
-      error: 'Invalid query parameters',
-    });
-    return;
-  }
+    if (
+      typeof chainId !== 'string' ||
+      typeof address !== 'string' ||
+      typeof stat !== 'string'
+    ) {
+      handleError(res, 400, 'Invalid query parameters');
+      return;
+    }
 
-  const statNumber = parseInt(stat, 10);
-  const statType = STAT_TYPES[statNumber];
+    const statNumber = parseInt(stat, 10);
+    const statType = STAT_TYPES[statNumber];
 
-  if (!statType) {
-    res.status(400).send({
-      error: 'Invalid stat type',
-    });
-    return;
-  }
+    if (!statType) {
+      handleError(res, 400, 'Invalid stat type');
+      return;
+    }
 
-  console.log({ chainId, address, statType });
-  
-  // Check to see if we have this collection saved in the db
-  const db = await feeder();
-  const query = { 
-    chainId: chainId,
-    address: address
-  };
-  const collection = await db.collection("NFTCollections").find(query).toArray();
-
-  if (collection.length === 0) {
-    // no collection found... let's create one
-    const insertCollection = await db.collection("NFTCollections").insertOne(query);
+    console.log({ chainId, address, statType });
     
-    // Get sales hisory and create one record per transaction
-    const salesHistory = await dune.getNFTCollectionSales(address, 10);
+    // Check to see if we have this collection saved in the db
+    const db = await feeder();
+    const query = { 
+      chainId: chainId,
+      address: address
+    };
+    const collection = await db.collection("nftCollections").findOne(query);
+    const queryLimit = 10000;
+    let done = false;
+    let collectionId: ObjectId;
     
-  } else {
-    // found the collection... let's just save the latest transactions
-    console.log('collection exists');
+    /**
+     * @todo: Add a status flag that lets us know the collection is waiting
+     * on sales history so there is no need to create a duplicate record. Don't
+     * do this until we improve the types in the dune util to support strict
+     * typed return params. e.g. block_time,nft_token_id,usd_amount.
+     */
+    if (collection === null) {
+      // no collection found... let's create one
+      const metadata = await reservoir.getCollectionMetadata(address);
+      const newCollection = await db.collection("nftCollections").insertOne({
+        ...query,
+        ...metadata,
+        createdAt: new Date()
+      });
+      collectionId = newCollection.insertedId;
+      db.collection("nftSales").createIndex({ nftCollection_id: 1 });
 
-    // First get the number of transactions already saved and use that for the offset
+    } else {
+      // found the collection...
+      collectionId = collection._id;
+    }
+
+    while (!done) {
+      // Count the existing records
+      const offset = await db.collection("nftSales").countDocuments({
+        nftCollection_id: collectionId
+      });
+
+      // Get a page of sales history
+      const nftSales = await dune.getNFTCollectionSales(
+        address, 
+        queryLimit, 
+        offset, 
+        'block_time,nft_token_id,usd_amount,tx_hash,buyer,seller');
+
+      // Quit if there is no more sales history to page through 
+      if (nftSales.length === 0) {
+        done = true;
+        break;
+      }
+
+      let sales = [];
+      for (const sale of nftSales) {
+        sales.push({
+          ...sale,
+          nftCollection_id: collectionId
+        });
+      }
+      await db.collection("nftSales").insertMany(sales);
+    }
+
+    res.send({
+      response: 'Collection update successful',
+    });
+  } catch (error) {
+    handleError(res, 400, error.message);
   }
-  console.log(collection);
-
-  res.send({
-    response: collection,
-  });
 });
 
 /*
  * Init
  */
-
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
 server.on('error', console.error);
+
+/**
+ * Clean Error Response
+ * 
+ * @param response Express response object
+ * @param statusCode The status code to return
+ * @param message The error message
+ */
+function handleError(response: express.Response, statusCode: number, message: string) {
+  const data = {
+    "timestamp": Date.now(),
+    "status": statusCode,
+    "error": message
+  };
+
+  console.error(data);
+  response.status(statusCode).send(data);
+  response.end();
+}
