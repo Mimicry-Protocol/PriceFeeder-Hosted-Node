a54260f4a400c0852170e8ba717a3afb0bb04697
Daniel Cortes <danielgcortes@gmail.com>
2022-11-06 03:02:49 -0800

    Remove dummy lib

--- libs/blah/src/lib/blah.spec.ts
+++
@@ -1,7 +0,0 @@
-import { blah } from './blah';
-
-describe('blah', () => {
-  it('should work', () => {
-    expect(blah()).toEqual('blah');
-  });
-});
--- libs/blah/src/lib/blah.ts
+++
@@ -1,3 +0,0 @@
-export function blah(): string {
-  return 'blah';
-}
